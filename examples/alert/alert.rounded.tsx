88ecbdd5c7172b5833691042ce8dbafdeafbce88
Faheem <138996989+mohammadfaheemmir@users.noreply.github.com>
2024-03-20 16:59:37 +0530

    Update alert.rounded.tsx

--- examples/alert/alert.rounded.tsx
+++ examples/alert/alert.rounded.tsx
@@ -9,7 +9,7 @@ import { Alert } from 'flowbite-react';
 function Component() {
   return (
     <Alert color="warning" rounded>
-      <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
+      <span className="font-medium">Info alert!</span> Ok.
     </Alert>
   );
 }
